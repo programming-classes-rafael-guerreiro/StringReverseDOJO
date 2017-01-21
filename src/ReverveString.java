6b15f2f3e22081c3535d05cf4462136a3747f19c
gpoleze <gabrielpferreira@MacBook-Pro-de-Gabriel.local>
2017-01-20 20:36:31 -0800

    raciocionio de resolucao do problema

--- src/ReverveString.java
+++ src/ReverveString.java
@@ -1,20 +1,25 @@
+//Criar objeto chamando classe
+//Possa valor inicial ou nao
+//Criar metodo reverso
+//Methodo toString
+
+
 
 public class ReverveString {
-	private String value;
+	private String phrase;
 	
 	//constructor
 	public ReverveString(String value){
-		this.value = value;
+		this.phrase = value;
 	}
 	
 	//Methods
 	public void reverse(){
 		char temp;
-		String resultado;
-		for (int i = 0; i < value.length()/2; i++) {
-			temp = value.charAt(i);
+		String resultado[phrase.lenght];
+		for (int i = 0; i < phrase.length()/2; i++) {
+			temp = phrase.charAt(i);
 			value.charAt(i) = '1';
 		}
 	}
-	
 }
