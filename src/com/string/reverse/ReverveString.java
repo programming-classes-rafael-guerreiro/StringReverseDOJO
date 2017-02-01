f6f221f83883143b5c52ac63272ab322051534b0
gpoleze <gabriel.poleze@gmail.com>
2017-01-31 21:11:02 -0800

    final version

--- src/com/string/reverse/ReverveString.java
+++ src/com/string/reverse/ReverveString.java
@@ -1,4 +1,4 @@
-package com.string.reverse;  
+package com.string.reverse;
 
 //Criar objeto chamando classe
 //Possa valor inicial ou nao
@@ -15,13 +15,44 @@ public class ReverveString {
 
 	// Methods
 	public String reverse() {
-		char[] temp = new char[initialPhrase.length()];
 		int phraseLength = initialPhrase.length();
-		for (int i = 0; i < (phraseLength + 1) / 2; i++) {
-			temp[i] = initialPhrase.charAt(phraseLength - 1 - i);
-			temp[phraseLength - 1 - i] = initialPhrase.charAt(i);
+		char[] reversedString = new char[phraseLength];
+		boolean hasSurrogate = false;
+		
+		
+		int middle = (phraseLength + 1) / 2;
+
+		for (int index = 0; index < middle; index++) {
+			int lastIndex = phraseLength - 1 - index;
+			
+			reversedString[index] = initialPhrase.charAt(lastIndex);
+			reversedString[lastIndex] = initialPhrase.charAt(index);
+
+			if (isSurragate(phraseLength, index))
+				hasSurrogate = true;
 		}
 
-		return new String(temp);
+		if (hasSurrogate)
+			reverseSurrogate(reversedString, phraseLength);
+
+		return new String(reversedString);
 	}
+
+	private void reverseSurrogate(char[] reversedString, int phraseLength) {
+		for (int i = 0; i < phraseLength; i++) {
+			if (Character.isSurrogate(reversedString[i])) {
+				char aux = reversedString[i];
+				reversedString[i] = reversedString[i + 1];
+				reversedString[i + 1] = aux;
+				i++;
+			}
+		}
+	}
+
+	private boolean isSurragate(int phraseLength, int i) {
+		return Character.isSurrogate(initialPhrase.charAt(i))
+				|| Character.isSurrogate(initialPhrase.charAt(phraseLength - 1 - i));
+	}
+	
+	
 }
